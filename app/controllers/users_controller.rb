c17a656c19306ecf40555f6f24894174812f046a
blainelawson <blaine.lawson@gmail.com>
2019-09-16 15:50:36 -0400

    Added route and action for adding issues to user--action is incomplete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,5 @@
 class UsersController < ApplicationController
+    def add_issue
+        byebug
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get "/get_current_user", :to => "sessions#get_current_user"
   delete "/logout", :to => "sessions#destroy"
 
+  patch "/add_user_issue", :to => "users#add_issue"
+  
   namespace :api do
     namespace :v1 do
       resources :issues
