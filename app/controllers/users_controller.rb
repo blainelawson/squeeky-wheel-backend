44e687035f215e3d24edd951fc95ec751e730725
blainelawson <blaine.lawson@gmail.com>
2019-09-19 10:04:22 -0400

    Updated routing to be more restful for deleting/adding user issues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,30 @@
-class UsersController < ApplicationController
+class Api::V1::UsersController < ApplicationController
     def add_issue
+        byebug
         @issue = Issue.find_by(id: params[:issueId])
-        # byebug
+        
         if @issue && !my_current_user.issues.include?(@issue)
             my_current_user.issues << @issue
-            # byebug
+
             my_current_user.save
             render json: @issue
         else 
             render json: {
                 error: "There was an issue with the issue"
+            }
+        end
+    end
+
+    def delete_user_issue
+        @issue = Issue.find_by(id: params[:issueId])
+
+        if @issue && my_current_user.issues.include?(@issue)
+            my_current_user.issues.delete(@issue)
+
+            render json: @issue
+        else
+            render json: {
+                error: "Something went TERRIBLY wrong. Abandon hope."
             }
         end
     end
--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ Rails.application.routes.draw do
   get "/get_current_user", :to => "sessions#get_current_user"
   delete "/logout", :to => "sessions#destroy"
 
-  patch "/add_user_issue", :to => "users#add_issue"
-  
   namespace :api do
     namespace :v1 do
       resources :issues
       resources :users do
+        patch "/add_user_issue", :to => "users#add_issue"
+        patch "/delete_user_issue", :to => "users#delete_user_issue"
         resources :issues
       end
     end
